c709cf431294fe390a07328b0cb04fb3149c34b6
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-24 20:06:07 +0530

    updated all code for service catalog

---
+++ modules/service_catalog/variables.tf
@@ -0,0 +1,60 @@
+variable "portfolio_name" {
+  description = "Name of the Service Catalog portfolio"
+  type        = string
+}
+
+variable "portfolio_description" {
+  description = "Description of the Service Catalog portfolio"
+  type        = string
+}
+
+variable "provider_name" {
+  description = "Name of the provider creating the portfolio"
+  type        = string
+}
+
+variable "product_name" {
+  description = "Name of the product in the portfolio"
+  type        = string
+}
+
+variable "product_owner" {
+  description = "Owner of the product"
+  type        = string
+}
+
+variable "template_url" {
+  description = "S3 URL to the CloudFormation template"
+  type        = string
+}
+
+variable "provisioning_name" {
+  description = "Name for the provisioning artifact"
+  type        = string
+}
+
+
+variable "template_constraint_parameters" {
+  description = "JSON-encoded parameters for the template constraint"
+  type        = map(any)
+  default     = {}
+}
+
+
+
+variable "tag_key" {
+  description = "TagOption key"
+  type        = string
+  default     = "env"
+}
+
+variable "tag_value" {
+  description = "TagOption value"
+  type        = string
+  default     = "dev"
+}
+
+variable "user_arn" {
+  description = "IAM principal ARN to associate with the portfolio"
+  type        = string
+}
---
+++ terraform.tfvars
@@ -0,0 +1,29 @@
+portfolio_name         = "instance Portfolio"
+portfolio_description  = "Portfolio for provisioning Ec2 instance"
+provider_name          = "niyaz"
+
+product_name           = "instance Product"
+product_owner          = "niyaz"
+template_url           = "https://demotestbucketforservicecatalog.s3.us-east-1.amazonaws.com/ec2_instance.yml"
+provisioning_name      = "v1"
+
+enable_template_constraint = true
+template_constraint_parameters = {
+  Rules = {
+      AllowedInstanceTypes = {
+        Assertions = [{
+          Assert = {"Fn::Contains" = [["t2.micro", "t3.micro"], {"Ref" = "InstanceType"}]}
+          AssertDescription = "Allowed instance types are t2.micro and t3.micro."
+        }]
+      }
+    }
+}
+
+enable_launch_constraint = true
+#launch_role_arn          = "arn:aws:iam::676206899900:role/servicecatalogaccesss"
+
+create_tag_option        = true
+tag_key                  = "env"
+tag_value                = "dev"
+
+user_arn                 = "arn:aws:iam::975049998898:user/service-usr"
